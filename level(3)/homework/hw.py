cbfd15a712ad1cfd4b1821d37ebddbb70340e3fa
Datobasiashvili <basiashvilidato@gmail.com>
2024-06-30 15:03:44 +0400

    third commit

---
+++ level(3)/homework/hw.py
@@ -0,0 +1,10 @@
+name = "Dato "
+age = 17
+secondName = "Basiashvili"
+dreamCar = "Ferrari"
+
+print("My name is " + name + secondName + ".")
+print("I am " + str(age) + " years old.")
+print("My dream car is " + dreamCar + ".")
+
+#დანარჩენი გვქონდა solo learn - ში

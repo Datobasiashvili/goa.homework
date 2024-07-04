00f34016c348ef96558aa56163658bcca40a9787
Datobasiashvili <basiashvilidato@gmail.com>
2024-07-04 21:10:48 +0400

    fourth commit

---
+++ level(4)/classwork/cw.py
@@ -0,0 +1,20 @@
+number1 = int(input("enter any number: "))
+number2 = int(input("enter any number: "))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+print(number1 ** number2)
+print(number1 // number2)
+
+name = input("enter your name: ")
+lastName = input("enter your last name: ")
+print("User is: " + name + " " + lastName)
+
+favoriteCity = input("enter your favorite city: ")
+favoriteFood = input("enter your favorite food: ")
+favoriteColor = input("enter your favorite Color: ")
+
+print(name + " " + lastName + " has a favorite city, it's " + favoriteCity + ". His favorite food is " + favoriteFood + " and his favorite color is "+ favoriteColor + ".")
---
+++ level(4)/homework/hw.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+lastName = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print("Hello " + name + ".")
+print(name + " " + lastName + " is " + str(age) + " years old. He lives in " + city + ".")
+
+#პითონში ჩვენ ვიყენებთ input ფუნქციას, რათა მომხმარებელმა შეძლოს მასში ინფორმაციის ჩაწერა.
+#შემდეგ ეს ინფორმაცია ტერმინალში გამოისახება , როგორც output.
